547c8400b2a04cfaef08a5cb7d076f0f20380cea
Oksana Paygildina <kazaikina1997@mail.ru>
2023-10-09 19:05:25 +0300

    delenie na 3

--- src/test/java/RestCalculationTest.java
+++ src/test/java/RestCalculationTest.java
@@ -4,10 +4,18 @@ import ru.netology.javaqa.Project6.RestCalculation;
 
 public class RestCalculationTest {
     @Test
-    public void testNumberMonths() {
+    public void testNumberMonths1() {
         RestCalculation service = new RestCalculation();
         int expected = 2;
         int actual = service.calcRest(100_000, 60_000, 150_000);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNumberMonths2() {
+        RestCalculation service = new RestCalculation();
+        int expected = 3;
+        int actual = service.calcRest(10_000, 3_000, 20_000);
         Assertions.assertEquals(expected, actual);
     }
 
